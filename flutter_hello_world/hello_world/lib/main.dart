2b2f1c5fa4f2b7b0ff9e3584d0bc1b857e4c55cf
itsmee3223 <tamvam.pemberani@gmail.com>
2022-03-18 22:47:23 +0700

    Stateless dan Stateful Widget

--- flutter_hello_world/hello_world/lib/main.dart
+++ flutter_hello_world/hello_world/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,13 +11,79 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello, world!'),
-        ),
         body: Center(
-          child: Text('Hello, world!'),
+          child: BiggerText(
+              text: "Hello world!"), // Ubah widget Heading ke PerubahanText
         ),
       ),
+    );
+  }
+}
+
+// Contoh stateful widget
+// class Heading extends StatelessWidget {
+//   final String text;
+
+//   Heading({required this.text});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       text,
+//       style: TextStyle(
+//         fontSize: 24.0,
+//         fontWeight: FontWeight.bold,
+//       ),
+//     );
+//   }
+// }
+
+// class ContohStateful extends StatefulWidget {
+//   final String parameterWidget; // ini parameter widget
+
+//   const ContohStateful({required this.parameterWidget});
+
+//   @override
+//   _ContohStatefulState createState() => _ContohStatefulState();
+// }
+
+// class _ContohStatefulState extends State<ContohStateful> {
+//   String _dataState;
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//   }
+// }
+
+// penerapan stateful widget
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({required this.text});
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
